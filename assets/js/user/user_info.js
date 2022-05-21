4e12123e9f227c71168b8ec84f7914168b169f4d
guzengpei <1093569539@qq.com>
2022-05-21 16:53:46 +0800

    用户功能完成

--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,4 +1,4 @@
-$(function() {
+$(function () {
     const form = layui.form;
     // 自定义校验规则
     form.verify({
@@ -7,17 +7,47 @@ $(function() {
         },
     });
 
-    const initUserinfo = function(){
+    const initUserinfo = function () {
         $.ajax({
             type: "GET",
-            url:'/my/userinfo',
-            success: function(res){
+            url: '/my/userinfo',
+            success: function (res) {
                 // console.log(res);
                 if (res.status !== 0) return layer.msg('获取用户信息失败')
                 layer.msg('获取用户数据成功')
+
+                form.val("formUserInfo", res.data);
             }
         })
     }
 
     initUserinfo()
+
+
+    // 实现点击重制
+    $('#btnReset').click(function (e) {
+        e.preventDefault()
+        initUserinfo()
+    })
+
+
+    //  更新用户信息
+    $('.layui-form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'POST',
+            url: '/my/userinfo',
+            data: $(this).serialize(),
+            success: res => {
+                if (res.status !== 0) return layer.msg('获取用户信息失败')
+                // layer.msg('获取用户数据成功')
+
+                // 调用index的头像渲染函数重新渲染页面
+                window.parent.getUserInfo()
+            }
+        })
+    })
+
+    
+
 })
